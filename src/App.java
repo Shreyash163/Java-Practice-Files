83523b2d9441d4afe77a495e511b2a3393b25b57
Shreyash163 <shreyashmemane511@gmail.com>
2023-06-10 23:48:57 +0530

    changes made to the new branch

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       System.out.println("Hello, NEW World!");
+       System.out.println("Hello,  World! , this code is written in new branch");
     }
 }
