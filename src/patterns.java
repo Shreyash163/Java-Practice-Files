7d5cf315e33a247678f166c81e85384abcb6887c
Shreyash163 <shreyashmemane511@gmail.com>
2023-06-15 00:21:04 +0530

    Square pattern

--- src/patterns.java
+++ src/patterns.java
@@ -1,49 +1,29 @@
 public class patterns {
     public static void main(String args[])
     {
-        int a ;
-        for(int i = 0 ; i<5 ; i++)
-        {
+        int a = 8;
+          for(int i=0 ; i<a ; i++)
+          {
 
-            
-            System.out.println("");
-            
-            for(int k = 0 ;k<5-i-1;k++)
+            for(int j=0 ; j<a ; j++)
             {
-                System.out.print(" ");
-            }
-            
-            for(int j=0 ; j<(2*i + 1)  ;j++)
-            {
-                System.out.print("*");
+                if(i==0 || j ==0 || i==a-1 || j==a-1)
+                {
+                    System.out.print("*");
                 }
-            for(int k = 0 ;k<5-i+1;k++)
-            {
-                System.out.print(" ");
-            }
-        }
-
-        for(int i = 5 ; i>0 ; i--)
-        {
-
-            
-            System.out.println("");
-            
-            for(int k = 0 ;k<5-i+1;k++)
-            {
-                System.out.print(" ");
-            }
-            
-            for(int j=0 ; j<(2*i - 1)  ;j++)
-            {
-                System.out.print("*");
+                else
+                {
+                    System.out.print(" ");
                 }
-            for(int k = 0 ;k<5-i+1;k++)
-            {
-                System.out.print(" ");
+                
             }
-            
-        }
+            System.out.println("");
+          }
+        
+
+       
+
+       
     }
     
 }
